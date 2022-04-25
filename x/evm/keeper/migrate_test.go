9c370155f6bde519514478736e849d8c8ddd9d22
Sammy Liu <sammy.liu@axelar.network>
2022-04-25 22:49:27 +0800

    feat/delete uaxl token on all evm chains for re-deployment

--- x/evm/keeper/migrate_test.go
+++ x/evm/keeper/migrate_test.go
@@ -5,7 +5,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramsKeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -15,7 +14,6 @@ import (
 	"github.com/axelarnetwork/axelar-core/testutils/rand"
 	"github.com/axelarnetwork/axelar-core/x/evm/types"
 	"github.com/axelarnetwork/axelar-core/x/evm/types/mock"
-	"github.com/axelarnetwork/axelar-core/x/evm/types/testutils"
 	"github.com/axelarnetwork/axelar-core/x/nexus/exported"
 )
 
@@ -28,7 +26,7 @@ func setup() (sdk.Context, types.BaseKeeper) {
 	return ctx, keeper
 }
 
-func TestGetMigrationHandler_deletePendingGateway(t *testing.T) {
+func TestGetMigrationHandler_deleteUaxlToken(t *testing.T) {
 	ctx, keeper := setup()
 	evmChains := []exported.Chain{
 		{
@@ -39,31 +37,56 @@ func TestGetMigrationHandler_deletePendingGateway(t *testing.T) {
 			Name:   "evm-2",
 			Module: types.ModuleName,
 		},
-		{
-			Name:   "evm-3",
-			Module: types.ModuleName,
-		},
 	}
+
 	nexus := mock.NexusMock{
 		GetChainsFunc: func(_ sdk.Context) []exported.Chain {
 			return evmChains
 		},
 	}
-	keeper.ForChain(evmChains[0].Name).(chainKeeper).setGateway(ctx, types.Gateway{Address: types.Address(common.BytesToAddress(rand.Bytes(20)))})
-	keeper.ForChain(evmChains[1].Name).(chainKeeper).setGateway(ctx, types.Gateway{Address: types.Address(common.BytesToAddress(rand.Bytes(20))), Status: types.GatewayStatusConfirmed})
-	keeper.ForChain(evmChains[2].Name).(chainKeeper).setGateway(ctx, types.Gateway{Address: types.Address(common.BytesToAddress(rand.Bytes(20))), Status: types.GatewayStatusPending})
+
+	uaxlToken := types.ERC20TokenMetadata{
+		Asset: "uaxl",
+		Details: types.TokenDetails{
+			Symbol: rand.NormalizedStr(5),
+		},
+		Status: types.Confirmed,
+	}
+	otherToken := types.ERC20TokenMetadata{
+		Asset: rand.NormalizedStr(5),
+		Details: types.TokenDetails{
+			Symbol: rand.NormalizedStr(5),
+		},
+		Status: types.Confirmed,
+	}
 
 	for _, chain := range evmChains {
-		keeper.ForChain(chain.Name).SetParams(ctx, testutils.RandomParams())
+		keeper.ForChain(chain.Name).(chainKeeper).setTokenMetadata(ctx, uaxlToken)
+		keeper.ForChain(chain.Name).(chainKeeper).setTokenMetadata(ctx, otherToken)
+
+		_, ok := keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataByAsset(ctx, uaxlToken.Asset)
+		assert.True(t, ok)
+		_, ok = keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataBySymbol(ctx, uaxlToken.Details.Symbol)
+		assert.True(t, ok)
+		_, ok = keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataByAsset(ctx, otherToken.Asset)
+		assert.True(t, ok)
+		_, ok = keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataBySymbol(ctx, otherToken.Details.Symbol)
+		assert.True(t, ok)
 	}
 
 	handler := GetMigrationHandler(keeper, &nexus)
-	handler(ctx)
+	err := handler(ctx)
 
-	_, actual := keeper.ForChain(evmChains[0].Name).GetGatewayAddress(ctx)
-	assert.True(t, actual)
-	_, actual = keeper.ForChain(evmChains[1].Name).GetGatewayAddress(ctx)
-	assert.True(t, actual)
-	_, actual = keeper.ForChain(evmChains[2].Name).GetGatewayAddress(ctx)
-	assert.False(t, actual)
+	assert.NoError(t, err)
+
+	for _, chain := range evmChains {
+		_, ok := keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataByAsset(ctx, uaxlToken.Asset)
+		assert.False(t, ok)
+		_, ok = keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataBySymbol(ctx, uaxlToken.Details.Symbol)
+		assert.False(t, ok)
+		_, ok = keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataByAsset(ctx, otherToken.Asset)
+		assert.True(t, ok)
+		_, ok = keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataBySymbol(ctx, otherToken.Details.Symbol)
+		assert.True(t, ok)
+	}
 }
